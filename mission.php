e7a987b63e0119b7cf7f449ee4bdf6c1a00a1cad
trainwizard <samuel_bolds@taylor.edu>
2014-04-08 23:06:52 -0400

    php file for mission gen

---
+++ mission.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+    <title>Lisp Interface Test</title>
+</head>
+<body>
+<h2>Lisp Interface Test</h2>
+<?php
+    print "testing clisp interface ...<br>\n";
+    $results = '';
+    $fp = popen("/usr/local/bin/clisp ./test.lsp", "r");
+    $results .= fread($fp, 1024);
+    pclose($fp);
+
+    print "\$results = '" . $results . ".'";
+    flush();
+?>
+</body>
+</html>
